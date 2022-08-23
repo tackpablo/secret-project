913eba24ee223db67048983e548bbdb97cf4f68c
Pablo <tackpablo@gmail.com>
2022-08-22 21:43:03 -0700

    add user model from user schema

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    PAY_TYPE = [
+        ('CASH', 'Cash'),
+        ('CARD', 'Card')
+    ]
+
+    """Model representing a user"""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=24)
+    password = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    payment_type = models.CharField(choices=PAY_TYPE, default='CARD')
+    is_driver = models.BooleanField()
+
+
+
+    
+
