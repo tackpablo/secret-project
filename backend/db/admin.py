ed540c51db9b98dc8fea4505085f512f3a930cac
Pablo <tackpablo@gmail.com>
2022-08-25 15:44:38 -0700

    register models

--- backend/db/admin.py
+++ backend/db/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import User, Car, Driver, Transaction, Trip, Location, Refund
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Car)
+admin.site.register(Driver)
+admin.site.register(Transaction)
+admin.site.register(Trip)
+admin.site.register(Location)
+admin.site.register(Refund)
