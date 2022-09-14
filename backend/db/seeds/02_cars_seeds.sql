b4f9230398fa50f174e71430bbfa3b327255f120
Pablo <tackpablo@gmail.com>
2022-09-14 15:21:46 -0700

    update database script and seeds for cars with correct quotes

--- backend/db/seeds/02_cars_seeds.sql
+++ backend/db/seeds/02_cars_seeds.sql
@@ -1,6 +1,6 @@
 -- 02_cars_seeds.sql
 
 INSERT INTO cars (user_id, brand, model, color, type) VALUES
-  (1, 'Honda', 'Civic', "black", 'Hatchback'),
-  (2, 'Toyota', 'Camry', "blue", 'Sedan')
+  (1, 'Honda', 'Civic', 'black', 'Hatchback'),
+  (2, 'Toyota', 'Camry', 'blue', 'Sedan')
 ;
