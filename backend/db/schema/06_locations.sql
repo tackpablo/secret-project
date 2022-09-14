5cb850d84e5bf8cba7883df8a1a519995d4ef213
Pablo <tackpablo@gmail.com>
2022-09-14 15:12:31 -0700

    update locations table to have same column name as trips table

--- backend/db/schema/06_locations.sql
+++ backend/db/schema/06_locations.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS locations CASCADE;
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY NOT NULL,
   trip_id INTEGER REFERENCES trips(id) ON DELETE CASCADE,
-  start_name VARCHAR(255) NOT NULL,
+  start_location VARCHAR(255) NOT NULL,
   start_latitude VARCHAR(255) NOT NULL,
   start_longitude VARCHAR(255) NOT NULL,
-  end_name VARCHAR(255) NOT NULL,
+  end_location VARCHAR(255) NOT NULL,
   end_latitude VARCHAR(255) NOT NULL,
   end_longitude VARCHAR(255) NOT NULL
 );
