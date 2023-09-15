518ad21d99ba8d3050b09929f04e4a45fb0a599e
Pablo <tackpablo@gmail.com>
2023-09-14 21:38:01 -0700

    Updated login error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,61 +12,57 @@ import CodeVerificationPage from "./components/CodeVerificationPage/CodeVerifica
 import HorizontalLinearStepper from "./components/Stepper/Stepper";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <NavBar />
-            </div>
-            <Routes>
-                <Route exact path="/" element={<MainPage />} />
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
 
-                <Route
-                    path="/call"
-                    element={
-                        localStorage.getItem("user") != null ? (
-                            <CallDriverList />
-                        ) : (
-                            <MainPage />
-                        )
-                    }
-                />
-                <Route
-                    path="/transaction"
-                    element={
-                        localStorage.getItem("user") != null ? (
-                            <TransactionsPage />
-                        ) : (
-                            <MainPage />
-                        )
-                    }
-                />
-                <Route
-                    path="/profile"
-                    element={
-                        localStorage.getItem("user") != null ? (
-                            <ProfilePage />
-                        ) : (
-                            <MainPage />
-                        )
-                    }
-                />
-                <Route
-                    path="/driver"
-                    element={
-                        localStorage.getItem("user") != null ? (
-                            <DriverPage />
-                        ) : (
-                            <MainPage />
-                        )
-                    }
-                />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<HorizontalLinearStepper />} />
-                <Route path="/verify" element={<CodeVerificationPage />} />
-                <Route path="/logout" element={<MainPage />} />
-            </Routes>
-        </Router>
-    );
+        <Route
+          path="/call"
+          element={
+            localStorage.getItem("user") != null ? (
+              <CallDriverList />
+            ) : (
+              <MainPage />
+            )
+          }
+        />
+        <Route
+          path="/transaction"
+          element={
+            localStorage.getItem("user") != null ? (
+              <TransactionsPage />
+            ) : (
+              <MainPage />
+            )
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            localStorage.getItem("user") != null ? (
+              <ProfilePage />
+            ) : (
+              <MainPage />
+            )
+          }
+        />
+        <Route
+          path="/driver"
+          element={
+            localStorage.getItem("user") != null ? <DriverPage /> : <MainPage />
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<HorizontalLinearStepper />} />
+        <Route path="/verify" element={<CodeVerificationPage />} />
+        <Route path="/logout" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
